cf611230218187cca917eea0dacd1fff499aa360
Tom Ames <tsames1@gmail.com>
2021-09-28 13:00:01 -0700

    cannot get req.body to not be undefined

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 
 const express = require('express');
 const methodOverride = require('method-override');
+const bodyParser = require('body-parser')
 const Pokemon = require('./models/pokemon.js');
 const PORT = 3000;
 const app = express();
@@ -12,7 +13,7 @@ const app = express();
  *  Middleware *
 *********************************/
 
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(methodOverride('_method'));
 app.use(express.static('public'))
 
@@ -39,12 +40,16 @@ app.delete('/pokemon/:id', (req,res) => {
 //Update Route
 app.put('/pokemon/:id', (req, res) => {
   console.log(Pokemon[req.params.id - 1].name);
-  console.log(req);
+  console.log(req.body);
   Pokemon[req.params.id - 1].name = req.body.name
   res.redirect('/pokemon');
 })
 
 //Create Route
+app.post('/pokemon', (req,res) => {
+  console.log(req.body);
+  res.redirect('/pokemon/new');
+})
 
 //Edit Route
 app.get('/pokemon/:id/edit', (req,res) => {
