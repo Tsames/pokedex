56b8a3b3a5a664ec8ff0572d47268766a023c79e
Tom Ames <tsames1@gmail.com>
2021-09-27 19:02:15 -0700

    added pokemon cards to the index.ejs

--- server.js
+++ server.js
@@ -18,10 +18,28 @@ app.use(express.static('public'))
  *  Routes *
 *********************************/
 
+//Index Route
 app.get('/pokemon', (req, res) => {
   res.render('index.ejs', { data: Pokemon });
 })
 
+//New Route
+app.get('/pokemon/new', (req,res) => {
+  res.render('new.ejs');
+})
+
+//Delete Route
+
+//Update Route
+
+//Create Route
+
+//Edit Route
+app.get('/pokemon/:id/edit', (req,res) => {
+  res.render('edit.ejs', { data: Pokemon[Number(req.params.id) - 1] })
+})
+
+//Show Route
 app.get('/pokemon/:id', (req, res) => {
   res.render('show.ejs', { data: Pokemon[Number(req.params.id) - 1] });
 })
