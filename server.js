b695cf53ded61ff3318f1b54806f638ec0e799e9
Tom Ames <tsames1@gmail.com>
2021-09-27 18:24:06 -0700

    added index and show route and configured the basics of the show.ejs file

--- server.js
+++ server.js
@@ -1,3 +1,35 @@
+/*********************************
+ *  Requirements *
+*********************************/
+
 const express = require('express');
-const app = express();
 const Pokemon = require('./models/pokemon.js');
+const PORT = 3000;
+const app = express();
+
+/*********************************
+ *  Middleware *
+*********************************/
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'))
+
+/*********************************
+ *  Routes *
+*********************************/
+
+app.get('/pokemon', (req, res) => {
+  res.render('index.ejs', { data: Pokemon });
+})
+
+app.get('/pokemon/:id', (req, res) => {
+  res.render('show.ejs', { data: Pokemon[Number(req.params.id) - 1] });
+})
+
+/*********************************
+ *  Listener *
+*********************************/
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}!`);
+})
